d3a62a73d24265ffa3fd2fcf52f22de1eaaad58c
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-04-18 18:56:02 +0530

    Update numberOfGoodPairs.cpp

--- numberOfGoodPairs.cpp
+++ numberOfGoodPairs.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int numIdenticalPairs(vector<int>& nums) {
-	int pairs = 0;
+    int pairs = 0;
     for(int i = 0;i < nums.size() - 1;i++){
         for(int j = i + 1;j < nums.size();j++){
             if(nums[i] == nums[j])pairs++;
