5557203a86dc6bf390c70e410d070bc837d5efc8
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-06-29 18:14:54 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.26%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[46];
+    int helper(int n){
+        if(n < 2)
+            return 1;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = helper(n - 1) + helper(n-2);
+    }
+    
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp));
+        return helper(n);
+    }
+};
