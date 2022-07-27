22300fcca8a6bfc50713a4658d8155f6c7d4be0b
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-07-27 15:28:03 +0530

    Time: 16 ms (92.86%), Space: 26.5 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,38 +11,25 @@
  */
 class Solution {
 public:
+    int preorderIndex;
+    map<int,int> inorderIndexMap;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        //using a helper function with start and end parameter for both 
-        // preorder and inorder
-        return buildTreeHelper(preorder,inorder,0,preorder.size()-1,0,inorder.size()-1);
-    }
-    TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, int preS, int preE, int inS, int inE){
-        //base condition
-        if(preS > preE) return NULL;
-        //root is in start of preorder
-        int rootData = preorder[preS];
-        //rootIndex is the index of rootData in inorder traversal
-        int rootIndex = -1;
-        for(int i = inS;i <= inE;i++){
-            if(inorder[i] == rootData){
-                rootIndex = i;
-                break; //no need to check further
-            }
+        preorderIndex = 0;
+        for(int i = 0;i < inorder.size();i++){
+            inorderIndexMap[inorder[i]] = i;
         }
-        //calculating value for left and right parts of traversals(needed to use             // recurion)
-        int lPreS = preS + 1;
-        int lInS = inS;
-        int lInE = rootIndex - 1;
-        int lPreE = (lInE - lInS + lPreS);
-        int rPreS = lPreE + 1;
-        int rPreE = preE;
-        int rInS = rootIndex + 1;
-        int rInE = inE;
-        //creating a new root node
+        
+        return helper(preorder,0,inorder.size() - 1);
+    }
+private: 
+        TreeNode* helper(vector<int> &preorder,int left,int right){
+        if(left > right) 
+            return NULL;
+        int rootData = preorder[preorderIndex++];
         TreeNode *root = new TreeNode(rootData);
-        //using recursion assigning both left and right child
-        root->left = buildTreeHelper(preorder,inorder,lPreS,lPreE,lInS,lInE);
-        root->right = buildTreeHelper(preorder,inorder,rPreS,rPreE,rInS,rInE);
+        
+        root->left = helper(preorder,left,inorderIndexMap[rootData] - 1);
+        root->right = helper(preorder,inorderIndexMap[rootData] + 1,right);
         return root;
     }
 };
