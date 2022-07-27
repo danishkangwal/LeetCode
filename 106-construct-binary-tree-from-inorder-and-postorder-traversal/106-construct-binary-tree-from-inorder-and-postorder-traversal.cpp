031d05aa7a60bbe4aab60d628dfa4c02669c515a
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-07-27 21:58:18 +0530

    Time: 25 ms (69.80%), Space: 26.5 MB (43.78%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,38 +11,27 @@
  */
 class Solution {
 public:
+    int postIdx;
+    unordered_map<int, int> inorderIndexMap;
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        //using a helper funtion with start and end parameter for both
-        // inorder and postorder
-        return buildTreeHelper(inorder,postorder,0,inorder.size()-1,0,postorder.size()-1);
-    }
-    TreeNode* buildTreeHelper(vector<int> &inorder, vector<int> &postorder, int inS,int inE,int postS,int postE){
-        //base condition
-        if(inS > inE) return NULL;
-        //rootData is at the end in postorder
-        int rootData = postorder[postE];
-        //rootIndex is index of rootData in inorder traversal
-        int rootIndex = -1;
-        for(int i = inS;i <= inE;i++){
-            if(inorder[i] == rootData){
-                rootIndex = i;
-                break;
-            }
+        postIdx = inorder.size()-1;
+        for(int i = 0; i < inorder.size(); i++){
+            inorderIndexMap[inorder[i]] = i;
         }
-        //calculating both left and right value for inorder and postorder(needed for         // recursion)
-        int linS = inS;
-        int linE = rootIndex - 1;
-        int lpostS = postS;
-        int lpostE = linE - linS + lpostS;
-        int rinS = rootIndex + 1;
-        int rinE = inE;
-        int rpostS = lpostE + 1;
-        int rpostE = postE - 1;
-        //creating a root node
-        TreeNode* root = new TreeNode(rootData);
-        //assigning left and right values using recursion
-        root->left = buildTreeHelper(inorder,postorder,linS,linE,lpostS,lpostE);
-        root->right = buildTreeHelper(inorder,postorder,rinS,rinE,rpostS,rpostE);
+        
+        return helper(postorder, 0, inorder.size()-1);
+    }
+
+    TreeNode* helper(vector<int>& postorder,int inStart,int inEnd) {
+        if(inStart > inEnd) 
+            return NULL;
+        
+        TreeNode* root = new TreeNode(postorder[postIdx--]);
+        int pivot = inorderIndexMap[root -> val];
+        
+        root -> right = helper(postorder, pivot+1, inEnd);
+        root -> left  = helper(postorder, inStart, pivot-1);
+        
         return root;
     }
 };
