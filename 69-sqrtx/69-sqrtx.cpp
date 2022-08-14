343a73f90bbfa70f125d775f83fe620219f7ba78
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-08-14 21:57:07 +0530

    Time: 8 ms (27.88%), Space: 5.9 MB (25.28%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long ans = 1;
-        while(ans*ans<=x)
-            ans++;
-        return ans-1;
+        int low = 1,high = x;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(x/mid == mid)
+                return mid;
+            else if(x/mid > mid)
+                low = mid+1;
+            else 
+                high = mid-1;
+        }
+        return high;
     }
 };
