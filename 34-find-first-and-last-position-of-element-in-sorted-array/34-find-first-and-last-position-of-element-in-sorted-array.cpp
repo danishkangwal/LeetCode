08b1443289b58e1e7f1b4db8215b1807804830ea
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-08-15 19:19:05 +0530

    Time: 8 ms (85.88%), Space: 13.8 MB (17.74%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0)
-            return {-1,-1};
-        if(nums.size()==1){
-            if(nums[0]==target)
+        if(nums.size()<2){
+            if(nums.size()&&nums[0]==target){
                 return {0,0};
-            else
-                return {-1,-1};
+            }
+            return {-1,-1};
         }
         int n = nums.size();
-        int lvalue = search(nums,target,0,n-1,0);
-        int rvalue = search(nums,target,0,n-1,1);
-        
+        int lvalue = search(nums,0,n-1,target,0);
+        int rvalue = search(nums,0,n-1,target,1);
+    
         return {lvalue,rvalue};
     }
-    int search(vector<int> &nums,int target,int low,int high,int toggle){
-        int res = -1;
+    int search(vector<int> &nums,int low,int high,int target,int toggle){
+        int ans = -1;
         while(low<=high){
-            int mid = low + (high-low)/2;
+            int mid = low + ((high-low)>>1);
             if(nums[mid]==target){
-                res = mid;
-                low = (toggle?mid+1:low);
-                high = (toggle?high:mid-1);
+                ans = mid;
+                low = toggle?mid+1:low;
+                high = toggle?high:mid-1;
             }
             else if(nums[mid]>target){
                 high = mid-1;
@@ -31,6 +29,6 @@ public:
                 low = mid+1;
             }
         }
-        return res;
+        return ans;
     }
 };
