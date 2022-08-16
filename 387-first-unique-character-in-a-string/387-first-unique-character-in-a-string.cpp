605ff0a8cb6b4b73ed3c18af3bc05c933f31977a
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-08-16 10:55:27 +0530

    Time: 24 ms (92.25%), Space: 10.5 MB (98.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);
-        int arr[26] = {0};
-        for (auto &ch : s) {
-            arr[ch-'a']++;
-        }
-        for (int i = 0; i < s.size(); i++) {
-            if (arr[s[i]-'a'] == 1) {
+        vector<int> freq(26,0);
+        for(auto ch:s)
+            freq[ch-'a']++;
+        for(int i = 0;i<s.size();i++){
+            if(freq[s[i]-'a'] == 1)
                 return i;
-            }
         }
+        
         return -1;
     }
 };
