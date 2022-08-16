f950efa224ada4049ba5a3c8c0baa94acd2e69fe
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-08-16 09:15:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.46%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long i = 1,temp = 0;
-        while(n>temp){
-            temp+=i;
-            i++;
+        int low = 1,high = n;
+        int res = 1;
+        while(low<=high){
+            long long mid = low+(high-low)/2;
+            if((mid+1)*mid/2<=n){
+                res = mid;
+                low = mid+1;
+            }
+            else if((mid+1)*mid/2>n){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        return temp==n?i-1:i-2;
+        return res;
     }
 };
