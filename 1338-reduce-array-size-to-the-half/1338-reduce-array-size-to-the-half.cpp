a991e00d8811a5e62d0e8d44b1cc4ce3addac636
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-08-19 19:27:56 +0530

    Time: 343 ms (51.00%), Space: 97.5 MB (5.75%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         unordered_map<int,int> m;
+        multiset<int,greater<int>> s;
         for(auto &i:arr)
             m[i]++;
-        vector<int> freq;
-        for(auto it=m.begin();it!=m.end();it++){
-            freq.push_back(it->second);
+        for(auto &i:m)
+            s.insert(i.second);
+        int ans = 0,count = 0;
+        for(auto it = s.begin();count<arr.size()/2;it++){
+            ans++;
+            count += *it;
         }
-        sort(freq.rbegin(),freq.rend());
-        int n=arr.size(),count = 0,rem = n,index = 0;
-        while(rem>n/2){
-            rem -= freq[index++];
-            count++;
-        }
-        return count;
+        return ans;
     }
 };
