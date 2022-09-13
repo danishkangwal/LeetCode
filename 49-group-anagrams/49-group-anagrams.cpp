747ee3ea17e1c2af5717a060f905db112951926a
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-09-13 16:25:01 +0530

    Time: 23 ms (99.82%), Space: 19.7 MB (77.21%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> copy=strs;
-        for(auto &i:copy){
-            sort(i.begin(),i.end());
-        }
+        unordered_map<string,vector<string>> m;
         vector<vector<string>> ans;
-        vector<bool> done(strs.size(),0);
-        for(int i=0;i<strs.size();i++){
-            vector<string> t;
-            if(done[i]==0)
-                t.push_back(strs[i]);
-            for(int j=i+1;j<strs.size();j++){
-                if(done[j]==0 && copy[i]==copy[j]){
-                    t.push_back(strs[j]);
-                    done[j]=1;
-                }
-            }
-            if(t.size()){
-                ans.push_back(t);
-            }
+        for(auto i:strs){
+            string tmp(i);
+            sort(tmp.begin(),tmp.end());
+            m[tmp].push_back(i);
+        }
+        for(auto it=m.begin();it!=m.end();it++){
+            ans.push_back(it->second);
         }
         return ans;
     }
