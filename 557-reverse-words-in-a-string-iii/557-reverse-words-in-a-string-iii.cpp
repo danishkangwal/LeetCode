ddddebf5eb982a2907ff59ee4ea949d73397c76e
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-09-22 15:20:27 +0530

    Time: 53 ms (14.19%), Space: 13.4 MB (19.40%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> v;
+        string ans="";
         string tmp="";
         for(auto ch:s){
             if(ch==' '){
-                v.push_back(tmp);
+                reverse(tmp.begin(),tmp.end());
+                ans+=tmp+' ';
                 tmp="";
             }else{
                 tmp+=ch;
             }
         }
-        v.push_back(tmp);
-        for(auto &x:v){
-            reverse(x.begin(),x.end());
-        }
-        s="";
-        for(int i=0;i<v.size()-1;i++){
-            s+=v[i];
-            s+=" ";
-        }
-        s+=v.back();
-        return s;
+        reverse(tmp.begin(),tmp.end());
+        ans+=tmp;
+        return ans;
     }
 };
