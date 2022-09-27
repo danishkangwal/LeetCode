908fd9705940f067fa5026949ce9900f13fd82d9
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-09-27 12:14:57 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.46%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> a(m+n);
-        int idx1=0,idx2=0,idx=0;
-        while(idx1<m&&idx2<n){
-            if(nums1[idx1]<nums2[idx2]){
-                a[idx++]=nums1[idx1++];
+        int idx1=m-1,idx2=n-1,idx=m+n-1;
+        while(idx1>=0 && idx2>=0){
+            if(nums1[idx1]>nums2[idx2]){
+                nums1[idx--]=nums1[idx1--];
             }else{
-                a[idx++]=nums2[idx2++];
+                nums1[idx--]=nums2[idx2--];
             }
         }
-        while(idx1<m)
-            a[idx++]=nums1[idx1++];
-        while(idx2<n)
-            a[idx++]=nums2[idx2++];
-        nums1=a;
+        while(idx2>=0)
+            nums1[idx--]=nums2[idx2--];
     }
 };
