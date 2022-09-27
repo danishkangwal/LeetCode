a6a6cb9f8324e194ae10122ca4a9ce687c6de1a7
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-09-27 11:28:42 +0530

    Time: 23 ms (28.84%), Space: 7.3 MB (51.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int a[26]={0};
+        for(int i=0;i<s.size();i++){
+            a[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+            a[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i])
+                return false;
+        }
+        return true;
     }
 };
