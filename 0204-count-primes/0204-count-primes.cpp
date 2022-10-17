c10126b60b6c00e7045630744d1ade63d6a052ba
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2022-10-17 08:05:01 +0530

    Time: 191 ms (94.41%), Space: 10.4 MB (28.16%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        return sieveOfEratosthenes(n);
-    }
-private:
-    int sieveOfEratosthenes(int n){
-        vector<bool> ISprime(n+1, true);
-        ISprime[0] = ISprime[1] = false;
-        for (int i = 2; i <= n; i++) 
-        {
-            if (ISprime[i] && (long long)i * i <= n) 
-            {
-                for (int j = i * i; j <= n; j += i)ISprime[j] =                                 false;
-            }
+        if(n<=2)
+            return 0;
+        vector<bool> prime(n+1,true);
+        prime[0]=0;
+        prime[1]=0;
+        for(int i=2;i*i<n;i++){
+            if (prime[i]) {
+				for (int j = i * i; j <= n; j += i) {
+					prime[j] = false;
+				}
+			}
         }
         int ans = 0;
-        for(int i = 2;i<n;i++)
-            if(ISprime[i])
+        for(int i=2;i<n;i++)
+            if(prime[i])
                 ans++;
         return ans;
-    }
-    bool isPrime(int n) {
-        if(n <= 1) 
-            return false;
-        if(n <= 3) 
-            return true;
-        if(n % 2 == 0 || n % 3 == 0) 
-            return false;
-        for(int i = 5; i * i <= n; i = i + 6)
-            if(n % i == 0 || n % (i + 2) == 0) 
-                return false;
-        return true;
     }
 };
