3b28367d0ca281deb4d7c7076a6fcf8f90c0d198
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-01-06 19:03:57 +0530

    Time: 176 ms (97.93%), Space: 80.6 MB (5.18%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
+        int max = *max_element(costs.begin(),costs.end());
+        vector<int> countMap(max+1);
+        for(auto& i:costs)
+            countMap[i]++;
         int count = 0;
-        for(auto &i:costs){
-            if(coins >= i){
-                count++;
-                coins -= i;
-            }
-            else{
+        for(int cost = 1;cost <= max; cost++){
+            if(countMap[cost] == 0)
+                continue;
+            if(cost > coins)
                 break;
-            }
+            int no = min(countMap[cost],coins/cost);
+            coins -= no*cost;
+            count += no;
         }
         return count;
     }
