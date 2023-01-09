9cf14c30f2f5658a73285a6fe49d92ffaf496ec2
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-01-09 09:08:24 +0530

    Time: 31 ms (93.62%), Space: 26 MB (58.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int temp = 0;
-        while(temp < nums.size()&&nums[temp]<0)
-            temp++;
-        if(temp == 0){
-            vector<int> squares(nums.size());
-            for(int i = 0;i < nums.size();i++){
-                squares[i] = nums[i]*nums[i];
-            }
-            return squares;
-        }
-        if(temp == nums.size()){
-            vector<int> squares(nums.size());
-            for(int i = nums.size()-1;i>=0;i--){
-                squares[nums.size()-1-i] = nums[i]*nums[i];
-            }
-            return squares;
-        }
-        vector<int> squares(nums.size());
-        int left = temp - 1,idx=0;
-        while(left>=0 && temp<nums.size()){
-            int leftSq = nums[left]*nums[left];
-            int rightSq = nums[temp]*nums[temp];
-            if(leftSq > rightSq){
-                squares[idx++] = rightSq;
-                temp++;
+        int i = 0,j = nums.size()-1,n = nums.size();
+        vector<int> squares(n);
+        int idx = n-1;
+        while(i<=j){
+            int left = nums[i]*nums[i];
+            int right = nums[j]*nums[j];
+            if(left > right){
+                squares[idx--] = left;
+                i++;
             }else{
-                squares[idx++] = leftSq;
-                left--;
-                }
-        }
-        while(left >= 0){
-            squares[idx++] = nums[left]*nums[left];
-            left--;
-        }
-        while(temp <= nums.size()-1){
-            squares[idx++] = nums[temp]*nums[temp];
-            temp++;
+                squares[idx--] = right;
+                j--;
+            }
         }
         return squares;
     }
