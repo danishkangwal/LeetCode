f6d7e0e42b461d3f5aa6789a8b87c00d27a9862c
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-01-23 14:54:40 +0530

    Time: 560 ms (33.68%), Space: 117 MB (67.71%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,38 +1,26 @@
 class MedianFinder {
 public:
     priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-        
+    priority_queue<int,vector<int>,greater<int>> minHeap;
     MedianFinder() {
+        
     }
     
     void addNum(int num) {
-        if(maxHeap.empty() || num < maxHeap.top()){
-            maxHeap.push(num);
-            if(maxHeap.size()-minHeap.size()>1){
-                minHeap.push(maxHeap.top());
-                maxHeap.pop();
-            }
-        }
-        else{
-            minHeap.push(num);
-            if(minHeap.size()-maxHeap.size()>1){
-                maxHeap.push(minHeap.top());
-                minHeap.pop();
-            }
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if(minHeap.size()>maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
     
     double findMedian() {
-        if(minHeap.size()==maxHeap.size()){
-            return (1.0*(minHeap.top()+maxHeap.top()))/2;
-        }
-        else if(minHeap.size()>maxHeap.size()){
-            return minHeap.top();
-        }
-        else{
+        if(maxHeap.size()>minHeap.size())
             return maxHeap.top();
-        }
+        else
+            return (minHeap.top()+maxHeap.top())/2.0;
     }
 };
 
