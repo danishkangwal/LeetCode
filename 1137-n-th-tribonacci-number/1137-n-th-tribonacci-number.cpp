b813a91b22d4b28f3ede209a077b48132a3eebac
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-01-30 23:35:57 +0530

    Time: 3 ms (38.83%), Space: 5.9 MB (94.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int a[38];
-        a[0] = 0;
-        a[1] = 1;
-        a[2] = 1;
-        for(int i = 3;i <= n;i++){
-            a[i] = a[i-1]+a[i-2]+a[i-3];
+        if(n==0)
+            return 0;
+        int n0 = 0;
+        int n1 = 1;
+        int n2 = 1;
+        for(int i = 0;i < n-2;i++){
+            int sum = n0+n1+n2;
+            n0 = n1;
+            n1 = n2;
+            n2 = sum;
         }
-        return a[n];
+        return n2;
     }
 };
