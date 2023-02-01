ffb50fa7342dfcdce002e946342da3432f51141b
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-01 22:05:59 +0530

    Time: 4 ms (61.25%), Space: 8 MB (29.64%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        map<string,int> mp;
-        for(int i = 0;i <str1.size();i++){
-            string prefix = str1.substr(0,str1.size()-i);
-            mp[prefix] = 1;
-        }
-        string ans = "";
-        for(int i = 0;i <str2.size();i++){
-            string prefix = str2.substr(0,str2.size()-i);
-            if(mp[prefix]&&prefix.size()>ans.size()){
-                if(helper(str1,prefix) and helper(str2,prefix))
-                    ans = prefix;
-            }
-        }
-        return ans;
-    }
-private:
-    bool helper(string s,string gcd){
-        if(s.size()==0)
-            return 1;
-        string temp = s.substr(0,gcd.size());
-        if(temp!=gcd)
-            return 0;
-        return helper(s.substr(gcd.size()),gcd);
+        if(str1==str2)
+            return str1;
+        if(str1+str2 != str2+str1)
+            return "";
+        if(str1.size()>str2.size())
+            str1 = str1.substr(str2.size());
+        if(str2.size()>str1.size())
+            str2 = str2.substr(str1.size());
+        return gcdOfStrings(str1,str2);
     }
 };
