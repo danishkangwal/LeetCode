1b8a0298ee9995dd078920211baac2adc40768da
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-01 22:18:39 +0530

    Time: 6 ms (50.18%), Space: 7.2 MB (65.71%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        return (str1+str2 != str2+str1)? "": str1.substr(0,gcd(str1.size(),str2.size()));
+        if(str1+str2 != str2+str1)
+            return "";
+        
+        int gcdLength = gcd(str1.size(),str2.size());
+        return str1.substr(0,gcdLength);
     }
 };
