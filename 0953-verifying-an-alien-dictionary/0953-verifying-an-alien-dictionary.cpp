be1b4dad950896f0af37402439633e063ccbd188
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-02 19:36:42 +0530

    Time: 5 ms (72.56%), Space: 9.4 MB (83.64%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        map<string,bool> mp;
-        for(auto& word:words){
-            for(auto&ch : word){
-                ch = 'a' + order.find(ch);
+        vector<int> orderMap(26);
+        for(int i =0;i < order.size();i++){
+            orderMap[order[i]-'a'] = i;
+        }
+        
+        for(int i = 0;i < words.size()-1;i++){
+            for(int j = 0;j < words[i].size();j++){
+                if(j >= words[i+1].size())
+                    return false;
+                
+                if(words[i][j] != words[i+1][j]){
+                    int curr = words[i][j]-'a';
+                    int next = words[i+1][j]-'a';
+                    
+                    if(orderMap[curr]>orderMap[next])
+                        return false;
+                    else
+                        break;
+                }
             }
-            mp[word] = 1;
         }
-        int idx = 0;
-        for(auto it = mp.begin();it != mp.end();it++){
-            if(it->first != words[idx++])
-                return 0;
-        }
-        return 1;
+        return true;
     }
 };
