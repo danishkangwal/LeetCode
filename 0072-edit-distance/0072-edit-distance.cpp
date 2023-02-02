5e881a69526896eb16d92fb0d99fa04345c1c103
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-02 10:36:54 +0530

    Time: 14 ms (68.04%), Space: 9 MB (41.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int minDistance(string a, string b) {
-        vector<vector<int>> v(a.size()+1,vector<int>(b.size()+1,-1));
-        return minDistance(a,b,v);
+        int n = a.size(),m = b.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+
+        for (int i = 0; i < n+1; ++i){
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < m+1; ++i){
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < n+1; ++i)
+        {
+            for (int j = 1; j < m+1; ++j)
+            {
+                if(a[n-i]==b[m-j]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
-    int minDistance(string a,string b,vector<vector<int>>& v){
-	if(a==b)
-		return 0;
-	int n = a.size(),m=b.size();
-	if(a=="" || b=="")
-		return max(n,m);
-	if(v[n][m] != -1)
-		return v[n][m];
-	
-    if(a[0]==b[0]){
-        return minDistance(a.substr(1),b.substr(1),v);
-    }
-	int ans = 0;
-	int x = 1+minDistance(a.substr(1),b,v);
-	int y = 1+minDistance(a.substr(1),b.substr(1),v);
-	int z = 1+minDistance(a,b.substr(1),v);
-	ans = min(x,min(y,z));
-	return v[n][m]=ans;
-}
 };
