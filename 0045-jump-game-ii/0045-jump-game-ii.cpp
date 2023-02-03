ce0c75568553ca161fcbf605e4f660187a84c9bb
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-03 14:59:11 +0530

    Time: 17 ms (74.36%), Space: 16.6 MB (83.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& a) {
-        int n = a.size();
-        vector<int> dp(n,a.size());
-
-        dp[n-1] = 0;
-        for(int i = n-2;i>=0;i--){
-            for(int j = 1;j <= a[i] && (i+j) < n;j++){
-                dp[i] = min(dp[i],1+dp[i+j]);
+        int jumps = 0,n=a.size();
+        int end=0,far=0;
+        
+        for(int i = 0;i < n-1;i++){
+            far = max(far,i+a[i]);
+            
+            if(i==end){
+                jumps++;
+                end = far;
             }
         }
-        return dp[0];
+        
+        return jumps;
     }
 };
