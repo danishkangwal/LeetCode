3f88b88e63c5ddde33e79df48408077d3e7eb204
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-03 14:12:49 +0530

    Time: 73 ms (55.47%), Space: 48.4 MB (79.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& a) {
-        int n = a.size();
-        vector<int> dp(n);
-        dp[n-1]=1;
+        int i=0,maxDistance = 0;
 
-        for(int i = n-2;i>=0;i--){
-            for(int j = 1;j <= a[i];j++){
-                if(dp[i+j]){
-                    dp[i] = 1;
-                    break;
-                }
+        while(i <= maxDistance){
+            maxDistance = max(maxDistance,i+a[i]);
+            if(maxDistance >= a.size()-1){
+                return true;
             }
-
+            i++;
         }
-        return dp[0];
+        return false;
     }
 };
