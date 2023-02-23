11d0b3e35a1dd20ad0b85a99ebd9eea8a7ecdf0a
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-02-23 12:38:05 +0530

    Time: 533 ms (65.83%), Space: 8.3 MB (7.37%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,16 +3,18 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size(),m = board[0].size();
 	
+        vector<vector<bool>> visited(n,vector<bool>(m,0));
         for (int i = 0; i < n; ++i)
         {
             for (int j = 0; j < m; ++j)
             {
                 if(board[i][j]!=word[0])
                     continue;
-                vector<vector<bool>> visited(n,vector<bool>(m,0));
                 visited[i][j] = 1;
                 if(wordSearch(board,word.substr(1),visited,i,j))
                     return 1;
+                visited.clear();
+                visited.resize(n,vector<bool>(m,0));
             }
         }
         return 0;
