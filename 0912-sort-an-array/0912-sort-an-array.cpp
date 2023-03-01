014ba7be081c1ee76e9c628ae7e9a9a9f433f796
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-01 13:41:24 +0530

    Time: 222 ms (51.41%), Space: 68.7 MB (35.06%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int> nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int> pq;
+        for(auto& i:nums)
+            pq.push(-1*i);
+        vector<int> res;
+        while(pq.size()){
+            res.push_back(-1*pq.top());
+            pq.pop();
+        }
+        return res;
     }
 };
