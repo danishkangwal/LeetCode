a2822f338e5cf21d1668f9ea425c4da20eda3e54
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-07 23:46:20 +0530

    Time: 322 ms (77.49%), Space: 94.6 MB (33.40%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -5,10 +5,13 @@ public:
         sort(time.begin(),time.end());
         unsigned long long low = 1,high = 1LL * totalTrips*time[0];
         while(low<high){
-            long mid = low + (high-low)/2;
+            long mid = (low+high)>>1;
             long total = 0;
-            for(auto i:time)
+            for(auto i:time){
                 total += mid/i;
+                if(total>=totalTrips)
+                    break;
+            }
             if(total < totalTrips)
                 low = mid+1;
             else
