21739a6af4ff294215cb6cbecbb930f2005be322
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-12 11:03:12 +0530

    Time: 43 ms (33.36%), Space: 31.3 MB (5.28%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,24 +9,65 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        ListNode* temp;
-        for(auto& list:lists){
-            temp = list;
-            while(temp){
-                pq.push(temp->val);
-                temp = temp->next;
-            }
+private:
+    ListNode* merge(ListNode** left,ListNode** right){
+        if(*left==NULL){
+            return *right;
+        }
+        if(*right==NULL){
+            return *left;
         }
         ListNode* head = new ListNode();
-        temp = head;
-        while(pq.size()){
-            temp->next = new ListNode(pq.top());
-            temp = temp->next;
-            pq.pop();
+        ListNode* tail = head;
+        ListNode* t1 = *left;
+        ListNode* t2 = *right;
+        while(t1 and t2){
+            if(t1->val < t2->val){
+                tail->next = t1;
+                t1 = t1->next;
+            }
+            else{
+                tail->next = t2;
+                t2 = t2->next;
+            }
+            tail = tail->next;
+        }
+        while(t1){
+            tail->next = t1;
+            tail = tail->next;
+            t1 = t1->next;
+        }
+        while(t2){
+            tail->next = t2;
+            tail = tail->next;
+            t2 = t2->next;
         }
         return head->next;
+    }
+    void print(ListNode* head){
+        while(head){
+            cout<<head->val;
+            head = head-> next;
+        }
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+            return NULL;
+        
+        if(lists.size()==1){
+            return lists[0];
+        }
+        
+        int mid = lists.size()/2;
+        
+        vector<ListNode*> left(lists.begin(),lists.begin()+mid);
+        vector<ListNode*> right(lists.begin()+mid,lists.end());
+        
+        
+        ListNode* leftSorted = leftSorted = mergeKLists(left);
+        ListNode* rightSorted = rightSorted = mergeKLists(right);
+        
+        return merge(&leftSorted,&rightSorted);
     }
 };
