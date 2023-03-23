42963d27cb04eec537dad8188ba3d8e07ba837cc
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-23 09:19:48 +0530

    Time: 132 ms (71.87%), Space: 51.7 MB (35.99%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,67 +1,29 @@
-struct DSU{
-private:
-    vector<int> parent, siz;
-public:
-    DSU(int n) {
-        siz.resize(n + 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; ++i)
-        {
-            parent[i] = i;
-            siz[i] = 1;
-        }
-    }
-
-    int findParent(int node) {
-        while (node != parent[node])
-            node = parent[node] = parent[parent[node]];
-        return node;
-    }
-
-    bool same(int u, int v) {
-        return findParent(u) == findParent(v);
-    }
-
-    void merge(int u, int v) {
-        int p1 = findParent(u);
-        int p2 = findParent(v);
-        if (p1 == p2)
-            return;
-        if (siz[p1] < siz[p2]) {
-            parent[p1] = p2;
-            siz[p2] += siz[p1];
-        }
-        else {
-            parent[p2] = p1;
-            siz[p1] += siz[p2];
-        }
-    }
-
-    int size(int node) {
-        return siz[findParent(node)];
-    }
-
-    int number() {
-        set<int> s;
-        for (int i = 0; i < parent.size()-1; ++i)
-        {
-            s.insert(findParent(i));
-        }
-        return s.size();
-    }
-};
-
 class Solution {
+    void dfs(vector<vector<int>>&adj,int node,vector<bool>&visited){
+        visited[node] = 1;
+        for(auto& i:adj[node]){
+            if(!visited[i]){
+                dfs(adj,i,visited);
+            }
+        }
+    }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(n-1>connections.size()){
+        if(n-1>connections.size()) 
             return -1;
+        vector<vector<int>> adj(n);
+        for(auto &i:connections){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        DSU d(n);
-        for(auto&i:connections){
-            d.merge(i[0],i[1]);
+        vector<bool> visited(n,0);
+        int connected = 0;
+        for(int i = 0;i<n;i++){
+            if(!visited[i]){
+                connected++;
+                dfs(adj,i,visited);
+            }
         }
-        
-        return d.number()-1;
+        return connected-1;
     }
 };
