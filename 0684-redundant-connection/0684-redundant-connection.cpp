af4aef3e3d23ee1d269a9221a7e81154b35268f0
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-27 14:24:26 +0530

    Time: 7 ms (77.90%), Space: 9.3 MB (32.47%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,64 +1,41 @@
-struct DSUsize {
-private:
-	vector<int> parent, siz;
-public:
-	DSUsize(int n) {
-		siz.resize(n + 1);
-		parent.resize(n + 1);
-		for (int i = 0; i <= n; ++i)
-		{
-			parent[i] = i;
-			siz[i] = 1;
-		}
-	}
-
-	int findParent(int node) {
-		while (node != parent[node])
-			node = parent[node] = parent[parent[node]];
-		return node;
-	}
-
-	bool same(int u, int v) {
-		return findParent(u) == findParent(v);
-	}
-
-	void merge(int u, int v) {
-		int p1 = findParent(u);
-		int p2 = findParent(v);
-		if (p1 == p2)
-			return;
-		if (siz[p1] < siz[p2]) {
-			parent[p1] = p2;
-			siz[p2] += siz[p1];
-		}
-		else {
-			parent[p2] = p1;
-			siz[p1] += siz[p2];
-		}
-	}
-
-	int size(int node) {
-		return siz[findParent(node)];
-	}
-
-	int number() {
-		set<int> s;
-		for (int i = 0; i < parent.size() - 1; ++i)
-		{
-			s.insert(findParent(i));
-		}
-		return s.size();
-	}
-};
 class Solution {
+    int cycleStart = -1;
+    bool *vis,*cycle,found;
+    void dfs(vector<int> adj[],int node,int parent){
+        if(vis[node]){
+            cycleStart = node;
+            found = true;
+           return;
+        }
+        vis[node] = 1;
+        for(auto& u:adj[node]){
+            if(u==parent)
+                continue;
+            if(!found)
+                dfs(adj,u,node);
+            if(cycleStart != -1)
+                cycle[node] = 1;
+            if(node == cycleStart){
+                cycleStart = -1;
+                return;
+            }
+        }
+        
+    }
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        DSUsize d(edges.size());
+        int n = edges.size();
+        cycle = (bool*)calloc(n+1,sizeof(bool));
+        vis = (bool*)calloc(n+1,sizeof(bool));
+        vector<int> adj[n+1];
         for(auto& edge:edges){
-            if(d.findParent(edge[0])==d.findParent(edge[1])){
-                return {edge[0],edge[1]};
-            }
-            d.merge(edge[0],edge[1]);
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        dfs(adj,1,-1);
+        for(int i = n-1;i>=0 ;i--){
+            if(cycle[edges[i][0]] and cycle[edges[i][1]])
+                return edges[i];
         }
         return {};
     }
