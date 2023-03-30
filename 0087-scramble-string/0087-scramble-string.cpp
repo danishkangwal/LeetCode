68910e0ff4d0c128b28965534a67fe4a9863f105
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-30 22:23:38 +0530

    Time: 257 ms (14.18%), Space: 53.1 MB (5.78%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
-    unordered_map<string,unordered_map<string,int>> mp;
+    unordered_map<string,bool> mp;
     //use 1 for true and -1 for false
     bool solve(string s1,string s2){
         if(s1==s2)
             return 1;
         if(s1.size()<=1)
             return 0;
-        if(mp[s1][s2]!=0)
-            return mp[s1][s2]==1;
+        if(mp.find(s1+" "+s2)!=mp.end())
+            return mp[s1+" "+s2];
         bool check = 0;
         for(int i = 1;i < s1.size();i++){
             //swap
@@ -24,8 +24,7 @@ class Solution {
                 break;
             }
         }
-        mp[s1][s2] = (check?1:-1);
-        return check;
+        return mp[s1+" "+s2] = check;
     }
 public:
     bool isScramble(string s1, string s2) {
