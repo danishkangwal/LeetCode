5a9f3f07bc491ae00bb255048c2e4346924fb6c9
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-03-31 09:14:43 +0530

    Time: 14 ms (89.92%), Space: 15.9 MB (78.00%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,33 +1,33 @@
 class Solution {
     unordered_map<string,bool> mp;
-    //use 1 for true and -1 for false
-    bool solve(string s1,string s2){
-        if(s1==s2)
-            return 1;
-        if(s1.size()<=1)
-            return 0;
-        if(mp.find(s1+" "+s2)!=mp.end())
-            return mp[s1+" "+s2];
-        bool check = 0;
-        for(int i = 1;i < s1.size();i++){
-            //swap
-            if(isScramble(s1.substr(0,i), s2.substr(s2.size()-i))
-              and isScramble(s1.substr(i),s2.substr(0,s2.size()-i))){
-                check = 1;
-                break;
-            }
-
-            //no swap
-            if(isScramble(s1.substr(0,i),s2.substr(0,i)) and
-              isScramble(s1.substr(i),s2.substr(i))){
-                check = 1;
-                break;
-            }
-        }
-        return mp[s1+" "+s2] = check;
-    }
 public:
     bool isScramble(string s1, string s2) {
-        return solve(s1,s2);
+        int n=s2.size();
+        if(s1==s2){
+            return true;
+        }
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        vector<int> c(26,0);
+        if(mp.count(s1+s2)){
+            return mp[s1+s2];
+        }
+        for(int i=1;i<=n-1;i++){
+            int j=n-i;
+            a[s1[i-1]-'a']++;
+            b[s2[i-1]-'a']++;
+            c[s2[j]-'a']++;
+            
+            if(a==b and isScramble(s1.substr(0,i),s2.substr(0,i)) and isScramble(s1.substr(i),s2.substr(i))){
+                mp[s1+s2]=true;
+                return true;
+            }
+            if(a==c and isScramble(s1.substr(0,i),s2.substr(j)) and isScramble(s1.substr(i),s2.substr(0,j))){
+                mp[s1+s2]=true;
+                return true;
+            }
+        }
+        mp[s1+s2]=false;
+        return false;
     }
 };
