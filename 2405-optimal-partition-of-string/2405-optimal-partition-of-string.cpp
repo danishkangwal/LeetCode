60299ca2bdb50495dd1fcdf0c09dedc05de62939
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-04-04 12:17:59 +0530

    Time: 222 ms (62.51%), Space: 44.7 MB (39.97%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int partitionString(string s) {
         int ans = 0;
-        unordered_map<int,bool> mp;
+        unordered_set<char> st;
         int i = 0;
         while(i < s.size()){
-            if(mp[s[i]-'a']){
+            if(st.count(s[i])){
                 ans++;
-                mp.clear();
+                st.clear();
             }
-            mp[s[i]-'a']=1;
+            st.insert(s[i]);
             i++;
         }
-        ans += mp.size()>0;
+        ans += st.size()>0;
         return ans;
     }
 };
