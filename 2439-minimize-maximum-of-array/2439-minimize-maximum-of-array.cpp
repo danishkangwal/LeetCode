313647bac05cc3a9471b163f496a5d23790ece67
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-05-08 12:36:41 +0530

    Time: 122 ms (91.07%), Space: 71.3 MB (88.06%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        
-        long long ans = 0, pref = 0;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            pref += nums[i];
-            ans = max(ans, (pref + i) / (i + 1));
+        long long ans = nums[0];
+        long long total = nums[0];
+        for(int i = 1; i < nums.size();i++){
+            total += nums[i];
+            ans = max(ans, 1LL * (total+i)/(i+1));
         }
-        
         return ans;
     }
 };
