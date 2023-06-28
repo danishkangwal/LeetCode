7df885ed182874b729d096daf31a4f382026bf41
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-06-28 12:22:38 +0530

    Time: 17 ms (12.89%), Space: 20.8 MB (26.82%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     string reverseWords(string str) {
-        vector<string> vs;
-	string tmp = "";
-	for(int i = 0;i < str.size();i++){
-		if(str[i]==' '){
-			if(tmp != "")
-				vs.push_back(tmp);
-			tmp = "";
-		}
-		else
-			tmp += str[i];
-	}
-	if(tmp != "")
-		vs.push_back(tmp);
-
-	string s = "";
-	for(int i = vs.size()-1;i >= 0;i--){
-		s += vs[i];
-		if(i>0)
-            s += ' ';
-	}
-	return s;
+        string s = "";
+        string tmp = "";
+        for(int i = 0;i < str.size();i++){
+            if(str[i]==' '){
+                if(tmp != ""){
+                    if(s != "")
+                        s = tmp + " " + s;
+                    else
+                        s = tmp;
+                }
+                tmp = "";
+            }
+            else
+                tmp += str[i];
+        }
+        if(tmp != ""){
+            if(s != "")
+                s = tmp + " " +s;
+            else
+                s = tmp;
+        }
+        return s;
     }
 };
